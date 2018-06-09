c5310e3382ee2fcb1178f05eb39e54ad218fa2e3
test.gluck <test@gluck>
2018-06-09 16:31:30 +0500

    Merge origin/narine
    Conflicts:
    	controllers/SiteController.php

--- modules/order/views/default/view.php
+++ modules/order/views/default/view.php
@@ -53,7 +53,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= $form->field($client, 'phone') ?>
 
             <?=
-            $form->field($client, 'personal_accept')->checkbox()->label('Я даю согласие на обратобку персональных данных. '
+            $form->field($client, 'personal_accept')->checkbox()->label('Я даю согласие на обработку персональных данных. '
                     . Html::a('Ознакомиться с условиями', ['/soglasie'], ['target' => '_blank'])
             )
             ?>
