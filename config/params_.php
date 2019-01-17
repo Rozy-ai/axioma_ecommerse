c0eaca6ee343fb4b04bffc465055675c90f3a422
test.gluck <test@gluck>
2019-01-17 16:00:10 +0500

    Merge origin/master
    Conflicts:
    	public_html/import/grid-export (5).xlsx

---
+++ config/params_.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'kpsmol@gmail.com',
+    'senderEmail' => 'noreply@axioma.pro',
+    'copyEmail' => 'firsov@axioma.pro',
+//    'baseDomain' => $_SERVER['SERVER_NAME'],
+    'defaultDomain' => 'axioma.pro',
+    'baseDomain' => 'axioma.pro',
+];
