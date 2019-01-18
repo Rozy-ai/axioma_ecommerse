df5ce2518315f4673cd061d1be3f484e9837e81f
test.gluck <test@gluck>
2019-01-18 19:57:29 +0500

    Merge origin/master
    Conflicts:
    	public_html/import/grid-export (5).xlsx

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -246,6 +246,7 @@ $isHome = (($controller->id === $default_controller) && ($controller->action->id
             </div>
         </noscript>
         <script type="text/javascript" src="//cdn.callbackhunter.com/cbh.js?hunter_code=51124a1b34a5d693228b07c3c7665145" charset="UTF-8"></script>
+        <script src="//static-login.sendpulse.com/apps/fc3/build/loader.js" sp-form-id="1ad15482e8b7662c1bfc899e478cab87074ce6a97fbdb78b5c8185ae61c53066"></script>
     </body>
 </html>
 <?php $this->endPage() ?>
