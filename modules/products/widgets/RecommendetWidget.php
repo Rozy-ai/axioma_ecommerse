6a6bca7170db20a028df08aabf17545be7827e3c
test.gluck <test@gluck>
2019-01-24 08:29:02 +0500

    Merge origin/master
    Conflicts:
    	public_html/import/grid-export (5).xlsx

--- modules/products/widgets/RecommendetWidget.php
+++ modules/products/widgets/RecommendetWidget.php
@@ -6,9 +6,6 @@ use yii\base;
 use yii\base\Widget;
 use app\modules\products\models\Product;
 
-/*
- * ALTER TABLE `product` ADD `show_in_recomended` INT(1) NULL COMMENT 'Показывать в рекомендуемых' AFTER `product_type`, ADD `recomended_sort` INT(3) NULL COMMENT 'Сортировка рекомендуемых(обратная)' AFTER `show_in_recomended`;
- */
 
 class RecommendetWidget extends Widget {
 
@@ -22,7 +19,7 @@ class RecommendetWidget extends Widget {
     public function run() {
 
         $model = Product::find()->where([
-//                            'show_in_recomended' => 1,
+                            'show_in_recomended' => 1,
                             'product_type' => $this->type,
                         ])
                         ->orderBy('recomended_sort DESC')
