e38370d1eb0a6a0ec3178139f65c69084c34b985
test.gluck <test@gluck>
2019-02-06 11:51:23 +0500

    Merge origin/master
    Conflicts:
    	public_html/import/grid-export (5).xlsx

--- modules/forms/widgets/views/rassrochka_form.php
+++ modules/forms/widgets/views/rassrochka_form.php
@@ -5,10 +5,9 @@ use yii\bootstrap\ActiveForm;
 use yii\widgets\MaskedInput;
 ?>
 
-<div class="rassrochka-form well">
+<div class="rassrochka-form col-xs-12 col-sm-offset-2 col-sm-8">
 
     <p class="h2">ЗАЯВКА НА РАССРОЧКУ</p>
-
     <?php
     $form = ActiveForm::begin([
                 'id' => 'rassrochka-form',
@@ -16,50 +15,54 @@ use yii\widgets\MaskedInput;
                 'action' => '/forms/default/rassrochka',
     ]);
     ?>
+    <div class="row">
 
-    <?=
-    $form->field($model, 'name_organization')->textInput([
-        'placeholder' => $model->getAttributeLabel('name_organization')
-    ])->label(false)
-    ?>
-    <?=
-    $form->field($model, 'inn')->textInput([
-        'placeholder' => $model->getAttributeLabel('inn')
-    ])->label(false)
-    ?>
-    <?=
-    $form->field($model, 'email')->textInput([
-        'placeholder' => $model->getAttributeLabel('email')
-    ])->label(false)
-    ?>
-    <?=
-    $form->field($model, 'summ')->textInput([
-        'placeholder' => $model->getAttributeLabel('summ')
-    ])->label(false)
-    ?>
-    <?=
-    $form->field($model, 'time')->dropDownList($model->_time)->label(false)
-    ?>
-    <?=
-    $form->field($model, 'phone')->widget(MaskedInput::className(), [
-        'mask' => '+7 (999) 999-9999',
-        'options' => ['placeholder' => 'ТЕЛЕФОН', 'class' => 'form-control'],
-    ])->label(false)
-    ?>
+        <div class="col-sm-8">
 
-    <!--<p class="star-text">*Поля обязательны для сохранения</p>-->
-    <p class="politic-text">Нажимая кнопку «Отправить» Вы соглашаетесь с
-        <a href="/politic">политикой конфеденциальности</a> сайта.</p>
-
-    <div class="form-group button-wrap">
-        <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+            <?=
+            $form->field($model, 'name_organization')->textInput([
+                'placeholder' => $model->getAttributeLabel('name_organization') . '... *'
+            ])->label(false)
+            ?>
+            <?=
+            $form->field($model, 'inn')->textInput([
+                'placeholder' => $model->getAttributeLabel('inn') . '... *'
+            ])->label(false)
+            ?>
+            <?=
+            $form->field($model, 'email')->textInput([
+                'placeholder' => $model->getAttributeLabel('email') . '... *'
+            ])->label(false)
+            ?>
+            <p class="star-text text-muted">* Поля обязательны для заполнения</p>
+            <p class="politic-text text-danger">Нажимая кнопку «Отправить» Вы соглашаетесь с
+                <a href="/politic">политикой конфеденциальности</a> сайта.</p>
+        </div>
+        <div class="col-sm-4">
+            <?=
+            $form->field($model, 'summ')->textInput([
+                'placeholder' => $model->getAttributeLabel('summ') . '... *'
+            ])->label(false)
+            ?>
+            <?=
+            $form->field($model, 'time')->dropDownList($model->_time)->label(false)
+            ?>
+            <?=
+            $form->field($model, 'phone')->widget(MaskedInput::className(), [
+                'mask' => '+7 (999) 999-9999',
+                'options' => ['placeholder' => 'ТЕЛЕФОН' . '... *', 'class' => 'form-control'],
+            ])->label(false)
+            ?>
+            <div class="form-group button-wrap">
+                <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+            </div>
+        </div>
     </div>
+
+    <?php ActiveForm::end(); ?>
+
 </div>
 
-
-<?php
-ActiveForm::end();
-?>
 
 
 
