841c6f5658e74a51f20fafd34327d4ee578d8047
test.gluck <test@gluck>
2019-02-07 09:40:36 +0500

    Merge origin/master
    Conflicts:
    	public_html/import/grid-export (5).xlsx

--- modules/content/widgets/views/menu_service.php
+++ modules/content/widgets/views/menu_service.php
@@ -1,9 +1,10 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 ?>
 
-<section class="menu-articles">
+<section class="menu-articles menu-services">
 
     <li class="list-group-item header">Статьи</li>
 
@@ -11,7 +12,25 @@ use yii\helpers\Html;
     foreach ($model as $item):
 
         $active = ($item->id == $current_id) ? ' active' : '';
+        ?>
 
+        <a href="<?= Url::to(['/' . $item->url]) ?>" class="list-group-item <?= $active ?>">
+            <div class="menu-articles-link row">
+
+                <div class="col-xs-2 image-wrap">
+                    <?=
+                    $item->image ? Html::img($item->imageService, ['class' => 'img img-responsive']) :
+                            Html::img('/image/advantages/about.svg', ['class' => 'img img-responsive'])
+                    ?>
+                </div>
+                <div class="col-xs-10 link-wrap">
+                    <?= $item->header ?>
+                </div>
+
+            </div>
+        </a>
+
+        <?php
         echo Html::a($item->header, ['/' . $item->url]
                 , ['class' => 'list-group-item' . $active]);
     endforeach;
