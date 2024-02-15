4949a43b1484c26460944cd0ada576e3007d2e15
Roy <roy@MacBook-Air-Roy.local>
2024-02-15 18:01:10 +0500

    city choose

--- modules/city/widgets/views/city_choice.php
+++ modules/city/widgets/views/city_choice.php
@@ -7,7 +7,8 @@ use yii\helpers\Html;
     <i class="fas fa-map-marker-alt"></i> 
     <a class="dropdown-toggle" href="/category/<?php echo (isset($item->uri)) ? $item->uri : ''; ?>" data-toggle="dropdown"><?= $current; ?>
         <!--<span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span>-->
-    </a> <i class="fas fa-angle-down"></i>
+        <i class="fas fa-angle-down"></i>
+    </a> 
     <ul class="dropdown-menu forAnimate" role="menu">
         <?php
         foreach ($links as $k => $link) :
